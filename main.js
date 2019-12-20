0f0a485cd112052bc6cb110052c042be3e94ee13
Jamie <jamielee3136@gmail.com>
2019-12-20 15:41:44 -0800

    smooth scrolling

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
-function openNav() {
-  document.getElementById("myNav").style.height = "100%";
+var topbutton = document.getElementById("top");
+var mq = window.matchMedia('@media only screen and (max-width: 600px)');
+console.log(mq);
+if (mq.matches){
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    fixedNav.style.display = "block";
+  } else {
+    fixedNav.style.display = "none";
+  }
+} else {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
 }
 
-function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
-}
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
